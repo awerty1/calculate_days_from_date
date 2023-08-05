3164e1b84213d437b1f31c16179e8ed1153efeee
pheno <testuniversity@yandex.ru>
2023-08-05 10:17:14 +0300

    1. added Style 2. refactored "add_days" for list 3. added list "start_dates", "num_days_list", "titles" 4. added hash_symbol and logic for has_symbol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from datetime import date, timedelta
 
-from colorama import Fore
+from colorama import Fore, Style
 
 
 def validate_date(date_str):
@@ -24,39 +24,57 @@ def count_days(start_date_str):
     return delta.days
 
 
-def add_days(start_date_str, days_to_add):
-    start_date = validate_date(start_date_str)
+def add_days(start_dates, days_to_add):
+    result_dates = []
+    for start_date_str in start_dates:
+        start_date = validate_date(start_date_str)
 
-    if not start_date:
-        return None
+        if not start_date:
+            return None
 
-    try:
-        # Adding the specified number of days to the initial date
-        new_date = start_date + timedelta(days=days_to_add)
-    except OverflowError:
-        print(f"{Fore.RED}The result exceeds the maximum or minimum date value that can be represented "
-              f"{Fore.BLUE}{start_date}{Fore.RESET}")
-        return None
+        for num_days in days_to_add:
+            try:
+                # Adding the specified number of days to the initial date
+                new_date = start_date + timedelta(days=num_days)
+                result_dates.append(new_date.isoformat())
+            except OverflowError:
+                print(f"{Fore.RED}The result exceeds the maximum or minimum date value that can be represented "
+                      f"{Fore.BLUE}{start_date}{Fore.RESET}")
+                return None
 
     # Converting the new date to a string and returning the result
-    return new_date.isoformat()
+    return result_dates
 
 
 if __name__ == '__main__':
     # Set your start date in YYYY, MM, DD format
-    start_date_str = "2023-07-30"
+    start_dates = ["2023-07-30", "2023-07-09", "2023-07-20"]
     # Number of days to calculate from the current date
-    num_days = 90
+    # num_days = 90
+    # List with multiple values
+    num_days_list = [88, 88, 15]
+    titles = ["phone 1", "phone 2", "court"]
+    mult = 2
+    mult_hash = 22
+    hash_symbol_top = Style.BRIGHT + Fore.WHITE + "#" * mult_hash + Style.RESET_ALL
 
     # You can use it to the console
     # start_date_str = input("Enter the start date (YYYY-MM-DD): ")
     # num_days = int(input("Enter the number of days: "))
 
-    result_date = add_days(start_date_str, num_days)
-    if result_date is not None:
-        days_passed = count_days(start_date_str)
-        if days_passed is not None:
-            print(f"You have chosen a date: {Fore.GREEN}{start_date_str}{Fore.RESET}")
-            print(f"Next payment date {Fore.RED}{result_date}{Fore.RESET}, "
-                  f"{Fore.RED}{days_passed}{Fore.RESET} days have passed!")
-            print(f"Don't forget to pay, otherwise you'll become poorer")
+    result_dates = add_days(start_dates, num_days_list)
+    if result_dates is not None:
+        for start_date_str, result_date, titles in zip(start_dates, result_dates, titles):
+            days_passed = count_days(start_date_str)
+            # algorithm for count #
+            length = len(titles)+2
+            hash_symbol_title = Style.BRIGHT + Fore.WHITE + "#" * length + Style.RESET_ALL
+            hash_symbol_bot = Style.BRIGHT + Fore.WHITE + "#" * (mult_hash * mult) + hash_symbol_title + Style.RESET_ALL
+
+            if days_passed is not None:
+                print(f"{hash_symbol_top} {titles} {hash_symbol_top}")
+                print(f"You have chosen a date: {Fore.GREEN}{start_date_str}{Fore.RESET}")
+                print(f"Next payment date {Fore.RED}{result_date}{Fore.RESET} , "
+                      f"{Fore.RED}{days_passed}{Fore.RESET} days have passed!")
+                print(f"Don't forget to pay, otherwise you'll become poorer")
+                print(f"{hash_symbol_bot}\n")
